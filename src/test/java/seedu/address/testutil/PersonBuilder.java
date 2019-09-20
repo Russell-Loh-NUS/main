1ebe2c91c0ffa87e617f1582eafe398d791523fd
Russell Loh <e0310763@u.nus.edu>
2019-09-21 02:27:47 +0800

    -Fix test cases

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -19,7 +19,7 @@ public class PersonBuilder {
     public static final String DEFAULT_NAME = "Alice Pauline";
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "alice@gmail.com";
-    public static final String DEFAULT_REMARK = "";
+    public static final String DEFAULT_REMARK = "She likes aardvarks.";
 
     private Name name;
     private Phone phone;
@@ -31,7 +31,7 @@ public class PersonBuilder {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
-        email = new Email(DEFAULT_REMARK);
+        remark = new Remark(DEFAULT_REMARK);
         tags = new HashSet<>();
     }
 
